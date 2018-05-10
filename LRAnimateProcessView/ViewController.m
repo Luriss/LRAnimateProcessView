5bbe6d19698262ab449c66e7228bda26baddd33a
Luriss <luowenbiao_92@163.com>
2018-05-10 15:30:46 +0800

    动态条纹进度条
    动态条纹进度条，以及节点进度。

--- LRAnimateProcessView/ViewController.m
+++ LRAnimateProcessView/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "LRAnimationProgress.h"
 
 @interface ViewController ()
 
@@ -16,6 +17,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    LRAnimationProgress *pv = [[LRAnimationProgress alloc] initWithFrame:CGRectMake(30, 100, 300, 20)];
+    pv.backgroundColor = [UIColor clearColor];
+    pv.layer.cornerRadius = 10;
+    pv.progressTintColors = @[LRColorWithRGB(0xce2b2c),LRColorWithRGB(0xff734d)];
+    pv.stripesWidth = 4;
+    pv.stripesAnimated = YES;
+    pv.hideStripes = NO;
+    pv.numberOfNodes = 2;
+    pv.hideAnnulus = NO;
+    
+    [pv setProgress:0.5 animated:YES];
+    [self.view addSubview:pv];
+    
+    
+    
+    LRAnimationProgress *pv2 = [[LRAnimationProgress alloc] initWithFrame:CGRectMake(30, 200, 300, 15)];
+    pv2.backgroundColor = [UIColor clearColor];
+    pv2.layer.cornerRadius = 10;
+    pv2.progressTintColors = @[LRColorWithRGB(0xce2b2c),LRColorWithRGB(0xff734d)];
+    pv2.hideStripes = YES;
+    pv2.numberOfNodes = 4;
+    pv2.hideAnnulus = NO;
+    
+    [pv2 setProgress:0.8 animated:YES];
+    [self.view addSubview:pv2];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
